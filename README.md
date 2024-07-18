dd6395bf5f57764298d64787990bf9c420b74fa8
Matias <rojasmatias409@gmail.com>
2024-07-17 21:09:48 -0300

    home redesign

---
+++ README.md
@@ -0,0 +1,3 @@
+# TechShop E-commerce
+
+------------
--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,17 +1,24 @@
 import { readFileSync } from "node:fs"
-import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url"
+import { dirname, join } from "node:path"
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const products = JSON.parse(readFileSync(join(__dirname, "../../data/products.json")))
 
 export function home(req, res) {
-    res.render("index", { products })
+
+    const onsales = products.filter(product => product.sale == true).slice(0, 5)
+
+    const featureds = products.filter(product => product.featured == true).slice(0, 5)
+
+    res.render("index", { onsales: onsales, featureds: featureds })
 }
+
 export function about(req, res) {
     res.render("about")
 }
+
 export function contact(req, res) {
     res.render("contact")
 }
