0055954f06a070ff60e4262b68ca5ecc9e881703
Matias <rojasmatias409@gmail.com>
2024-07-23 21:13:05 -0300

    fix products filters

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -8,68 +8,86 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const products = JSON.parse(readFileSync(join(__dirname, "../../data/products.json")))
 const categories = [...new Set(products.map(product => product.category))]
 
-export function all(req, res){
-    const {search, min, max, category, onsale, freeshipping} = req.query
+export function all(req, res) {
+    const { search, min, max, category, onsale, freeshipping, sortby } = req.query
 
-    
+
     let productsFiltered = products
-    
-    if(search){
-        productsFiltered = productsFiltered.filter(product => product.name.toLowerCase().includes(search.toLowerCase()))  
+
+    if (sortby) {
+        switch (sortby) {
+            case "price-asc":
+                productsFiltered = productsFiltered.sort((a, b) => a.price - b.price)
+                break;
+            case "price-desc":
+                productsFiltered = productsFiltered.sort((a, b) => b.price - a.price)
+                break;
+            case "name-asc":
+                productsFiltered = productsFiltered.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()))
+                break;
+            case "name-desc":
+                productsFiltered = productsFiltered.sort((a, b) => b.name.toLowerCase().localeCompare(a.name.toLowerCase()))
+                break;
+        }
     }
 
-    if(min && min>0){
+    if (search) {
+        productsFiltered = productsFiltered.filter(product => product.name.toLowerCase().includes(search.toLowerCase()))
+    }
+
+    if (min && min > 0) {
         productsFiltered = productsFiltered.filter(product => {
             let price = product.price
-            
-            if(product.onsale){
-                price = (price * product.discount)/100
+
+            if (product.sale) {
+                price = (price * product.discount) / 100
             }
 
             return price >= min
-        })  
+        })
     }
 
-    if(max && max>0){
+    if (max && max > 0) {
         productsFiltered = productsFiltered.filter(product => {
             let price = product.price
-            
-            if(product.onsale){
-                price = (price * product.discount)/100
+
+            if (product.sale) {
+                price = (price * product.discount) / 100
             }
 
             return price <= max
-        })  
+        })
     }
-    
-    if(freeshipping && onsale){
+
+    if (freeshipping && onsale) {
         productsFiltered = productsFiltered.filter(product => product.shipping && product.sale)
-    }else if(freeshipping){
+    } else if (freeshipping) {
         productsFiltered = productsFiltered.filter(product => product.shipping)
-    }else if(onsale){
+    } else if (onsale) {
         productsFiltered = productsFiltered.filter(product => product.sale)
     }
-    
-    if(category){
+
+    if (category) {
         productsFiltered = productsFiltered.filter(product => product.category == category)
     }
 
 
     res.render("products", {
-        products: productsFiltered, 
+        products: productsFiltered,
         categories,
         minQuery: min,
         maxQuery: max,
         searchQuery: search,
         categoryQuery: category,
         onsaleQuery: onsale,
-        freeshippingQuery: freeshipping
+        freeshippingQuery: freeshipping,
+        sortByQuery: sortby
     })
 }
 
-export function details(req, res){
+export function details(req, res) {
     const id = req.params.id
     const product = products.find(product => product.id == id)
-    
-    res.render("details", {product})
+
+    res.render("details", { product })
 }
